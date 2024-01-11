25d66f07b7a94beff45286166c4fbe7120657c96
Elie Lowenstein <elielowenstein@elies-air.mynetworksettings.com>
2024-01-11 16:29:04 -0500

    completed P0

--- groupme-bot/bot.py
+++ groupme-bot/bot.py
@@ -10,6 +10,8 @@ BOT_ID = os.getenv("BOT_ID")
 GROUP_ID = os.getenv("GROUP_ID")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 LAST_MESSAGE_ID = None
+USER_ID = "96346276"
+API_KEY = os.getenv("API_KEY")
 
 
 def send_message(text, attachments=None):
@@ -38,12 +40,46 @@ def process_message(message):
     """Process and respond to a message."""
     global LAST_MESSAGE_ID
     text = message["text"].lower()
+    name = message["name"]
+    sender_id = message["sender_id"]
+    print(name)
+    print(text)
+    print(message)
+    
+    # Dont respond to any bots!
+    if message["sender_type"] != "bot":
+        # TASK 1: Respond to just me
+        if sender_id == USER_ID and text == "hey bot":
+            send_message("sup")
 
-    # i.e. responding to a specific message (note that this checks if "hello bot" is anywhere in the message, not just the beginning)
-    if "hello bot" in text:
-        send_message("sup")
+        # TASK 2: respond good morning/night to anyone
+        if sender_id != "system":   
+            if "good morning" == text:
+                send_message(f"good morning, {name}")
+            elif "good night" == text:
+                send_message(f"good night, {name}")
+
+        # TASK 3: new functionality- prompt bot to tell you a joke
+        if text == "tell me a joke":
+            send_message(get_joke())
+
+        # EXTRA CREDIT
+        if text == "tell me a fact":
+            send_message(get_fact())
+        
 
     LAST_MESSAGE_ID = message["id"]
+
+def get_joke():
+    #use ninja API to retreive a joke and send it into the chat
+    api_url = 'https://api.api-ninjas.com/v1/jokes?limit=1'
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+    return response.json()[0]["joke"]
+
+def get_fact():
+    api_url = 'https://api.api-ninjas.com/v1/facts?limit=1'
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+    return response.json()[0]["fact"]
 
 
 def main():
